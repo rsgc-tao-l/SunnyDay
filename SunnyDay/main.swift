1708976dda5baeea8dd6dadf959fb58bd2722307
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-03 07:48:13 -0500

    Initial Commit

---
+++ SunnyDay/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SunnyDay
+//
+//  Created by Russell Gordon on 2/3/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
