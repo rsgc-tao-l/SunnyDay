41adcf632b6c50c40dc6020d2f745196687b42b7
Student <student@macbook-pro-0435-luke-tao.local>
2017-02-13 10:48:42 -0500

    version finished

--- SunnyDay/SunnyDay.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ SunnyDay/SunnyDay.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -16,11 +16,89 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 500, height: 400)
 
-// Draw a circle in the middle of the canvas without a fill
+//rectangel blue background
+canvas.fillColor = Color(hue: 195, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 400)
+
+//x loop
+canvas.defaultLineWidth = 5
+for x in stride(from: 0, through: 500, by: 50) {
+    if x % 100 == 0 {
+        canvas.lineColor = Color.yellow
+    }else{
+        canvas.lineColor = Color.orange
+    }
+    
+    canvas.drawLine(fromX: 0, fromY: 400, toX: x, toY: 0)
+}
+
+
+//Y loop
+canvas.defaultLineWidth = 5
+for x in stride(from: 0, through: 400, by: 50) {
+    if x % 100 == 0 {
+        canvas.lineColor = Color.yellow
+    }else{
+        canvas.lineColor = Color.orange
+    }
+    
+    canvas.drawLine(fromX: 0, fromY: 400, toX: 500, toY: x)
+}
+// sun
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+canvas.fillColor = Color.yellow
+canvas.drawEllipse(centreX: 0, centreY: 400, width: 100, height: 100)
+// red circle
+canvas.borderColor = Color.red
 canvas.drawShapesWithBorders = true
 canvas.drawShapesWithFill = false
-canvas.defaultBorderWidth = 5
-canvas.drawEllipse(centreX: canvas.width / 2, centreY: canvas.height / 2 , width: 100, height: 100)
+canvas.defaultBorderWidth = 20
+canvas.drawEllipse(centreX: canvas.width / 2, centreY: -73 , width: 500, height: 500)
+//orange
+canvas.borderColor = Color.orange
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.defaultBorderWidth = 20
+canvas.drawEllipse(centreX: canvas.width / 2, centreY: -73 , width: 465, height: 465)
+
+//yellow
+canvas.borderColor = Color.yellow
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.defaultBorderWidth = 20
+canvas.drawEllipse(centreX: canvas.width / 2, centreY: -73 , width: 430, height: 430)
+
+//green
+canvas.borderColor = Color.green
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.defaultBorderWidth = 20
+canvas.drawEllipse(centreX: canvas.width / 2, centreY: -73 , width: 395, height: 395)
+
+//blue
+canvas.borderColor = Color.blue
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.defaultBorderWidth = 20
+canvas.drawEllipse(centreX: canvas.width / 2, centreY: -73 , width: 365, height: 365)
+//purple
+canvas.borderColor = Color.purple
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.defaultBorderWidth = 20
+canvas.drawEllipse(centreX: canvas.width / 2, centreY: -73 , width: 330, height: 330)
+
+//cloud 1
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+canvas.drawEllipse(centreX: 450, centreY: 400, width: 75, height: 75)
+//cloud 2
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+canvas.drawEllipse(centreX: 500, centreY: 375, width: 75, height: 75)
 
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
